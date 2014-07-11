8455ba5dd38b64da0d18d133027eaae01b270719
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-11 20:30:53 +0800

    Added field flags on poll.Polls

--- pollsite/polls/models.py
+++ pollsite/polls/models.py
@@ -1,16 +1,35 @@
 import datetime
+from django.template.defaultfilters import slugify
 from django.utils import timezone
 from django.db import models
 
 class Poll(models.Model):
+    slug = models.SlugField(null=True, blank=True)
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
+    flags = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.question
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    def was_flagged(self):
+        return self.flags > 0
+
+    was_flagged.admin_order_field = 'flags'
+    was_flagged.boolean = False
+    was_flagged.short_description = 'Was flagged?'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.s = slugify(self.question)
+        super(Poll, self).save(*args, **kwargs)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
