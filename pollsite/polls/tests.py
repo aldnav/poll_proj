4ff4efd60f1765f0e38676abbdf7ac6751db86d3
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-11 23:24:55 +0800

    Learned how to use test cases.

--- pollsite/polls/tests.py
+++ pollsite/polls/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from polls.models import Poll
+
+class PollMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_poll(self):
+        """
+        An expected bug a.k.a days of the future past!
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
