e12c386b7d1237479e09ec0139c64409e6192af2
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-11 18:48:45 +0800

    Added "polls" to installed apps as a local app.

--- pollsite/pollsite/settings.py
+++ pollsite/pollsite/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
+    'south',
+    # local apps
+    'polls'
 )
 
 MIDDLEWARE_CLASSES = (
