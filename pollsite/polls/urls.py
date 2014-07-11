ae29b3902705f505ef83b46f779fc082521c5c69
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-11 23:13:09 +0800

    Route to class-based views

--- pollsite/polls/urls.py
+++ pollsite/polls/urls.py
@@ -4,11 +4,13 @@ from polls import views
 
 urlpatterns = patterns('',
 	# /polls/
-	url(r'^$', views.index, name='index'),
+	# url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	# /polls/1
-	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/$', views.detail, name='detail'),
+	# url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/$', views.DetailView.as_view(), name='detail'),
 	# /polls/a-question,1/results/
-	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/results/$', views.results, name='results'),
+	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 	# /polls/a-question,1/vote/
 	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
