f4c8f81204ee3f8c9f3872a76da936ea9e712b6f
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-11 20:31:35 +0800

    Added flag report

--- pollsite/polls/admin.py
+++ pollsite/polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 # Register your models here.
-admin.site.register(Poll)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,			{'fields': ['question','flags']}),
+		('Date info', 	{'fields': ['pub_date']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question', 'pub_date', 'was_published_recently', 'was_flagged')
+	list_filter = ['pub_date']
+	search_fields = ['question', 'pub_date']
+
+admin.site.register(Poll, PollAdmin)
