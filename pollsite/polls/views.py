c2fab6c8ed609c32caa6fdd716706a8fe04c7631
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-11 23:16:30 +0800

    Added class-based views for poll:index, poll:detail and poll:results

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Poll, Choice
 
 # Create your views here.
-def index(request):
-    latest_polls = Poll.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_polls': latest_polls,
-    }
-    return render(request, 'polls/index.html', context)
 
-def detail(request, poll_slug, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
-    context = {
-        'poll': poll,
-    }
-    return render(request, 'polls/detail.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_polls'
 
-def results(request, poll_slug, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll':poll})
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(Poll, pk=self.kwargs['poll_id'])
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(Poll, pk=self.kwargs['poll_id'])
 
 def vote(request, poll_slug, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
@@ -40,3 +44,26 @@ def vote(request, poll_slug, poll_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results', args=(poll.slug, poll.id)))
+
+
+
+# def index(request):
+#     latest_polls = Poll.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_polls': latest_polls,
+#     }
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, poll_slug, poll_id):
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+#     context = {
+#         'poll': poll,
+#     }
+#     return render(request, 'polls/detail.html', context)
+
+# def results(request, poll_slug, poll_id):
+#     poll = get_object_or_404(Poll, pk=poll_id)
+#     return render(request, 'polls/results.html', {'poll':poll})
