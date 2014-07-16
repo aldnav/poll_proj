8f68bf0940410367530142579b77771e0e90fc8f
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-16 11:20:08 +0800

    Added detail route by simple function detail.

--- pollsite/polls2/urls.py
+++ pollsite/polls2/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from polls2 import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	# /polls2/
+	url(r'^$', views.index, name='index'),
+	# /polls2/first-question,1
+	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/$', views.detail, name='detail'),
 )
--- pollsite/polls2/views.py
+++ pollsite/polls2/views.py
@@ -1,5 +1,19 @@
+from django.http import Http404
+from django.utils import timezone
 from django.shortcuts import render
+from polls2.models import Poll, Choice, Voter
 
-# Create your views here.
-def index(request):
-	return render(request, 'polls2/index.html')
+def index(request):	
+	context = {}
+	context['latest_poll_list'] = Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+	return render(request, 'polls2/index.html', context)
+
+def detail(request, poll_slug, poll_id):
+	try:
+		poll = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+	context = {
+		'poll': poll,
+	}
+	return render(request, 'polls2/detail.html', context)
