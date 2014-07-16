1a1a4e0312184f6ae7ff602bfc31ddca85f69756
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-16 12:03:24 +0800

    Added url for votes and results

--- pollsite/polls2/urls.py
+++ pollsite/polls2/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	# /polls2/first-question,1
 	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/$', views.detail, name='detail'),
+	# /polls2/first-question,1/vote/
+	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+	# /polls2/first-question,1/results/
+	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 )
