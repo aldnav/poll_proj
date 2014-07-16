3428c794094feb187ee5b9d6d9458559ad7ed866
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-16 13:16:27 +0800

    Change def routes to Class-based views

--- pollsite/polls2/urls.py
+++ pollsite/polls2/urls.py
@@ -4,9 +4,9 @@ from polls2 import views
 
 urlpatterns = patterns('',
 	# /polls2/
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	# /polls2/first-question,1
-	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/$', views.detail, name='detail'),
+	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/$', views.DetailView.as_view(), name='detail'),
 	# /polls2/first-question,1/vote/
 	url(r'^(?P<poll_slug>[-\w]+),(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 	# /polls2/first-question,1/results/
