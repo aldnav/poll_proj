5215f9a4a0fcdf8d768447e557340351ee19fa5d
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-16 13:26:18 +0800

    Added tests for PollMethod

--- pollsite/polls2/tests.py
+++ pollsite/polls2/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from polls2.models import Poll
+
+class PollMethodTest(TestCase):
+
+    def test_was_published_recently_with_future_poll(self):
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
