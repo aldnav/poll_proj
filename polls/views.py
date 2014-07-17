8f017808780354c6f38157ce6883db052b0c93a5
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-17 14:53:18 +0800

    Removed get_object() since generic views use pk as id

--- polls/views.py
+++ polls/views.py
@@ -16,15 +16,18 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['trending_polls'] = Poll.objects.order_by('-answers')[:10]
+        context['trending_polls'] = Poll.objects.filter(answers__gte=5).order_by('-answers')[:10]
         return context
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
 
-    def get_object(self, *args, **kwargs):
-        return get_object_or_404(Poll, pk=self.kwargs['poll_id'])
+    # def get_object(self, *args, **kwargs):
+        # return get_object_or_404(Poll, pk=self.kwargs['poll_id'])
+
+    def get_queryset(self, *args, **kwargs):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Poll
