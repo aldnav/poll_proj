81e6077511c418e8bbb5cd8708b588d0c16d0130
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-07-17 11:42:55 +0800

    Added routes for votes, and results on poll.urls

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
     #/polls/1
     url(r'^(?P<poll_id>\d+)/$', views.DetailView.as_view(), name='detail'),
+    #/polls/1/results
+    url(r'^(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    #/polls/1/vote
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
