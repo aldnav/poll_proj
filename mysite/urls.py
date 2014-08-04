fa06723844299890e373f82aa872f628c2c13441
aldnav <aldrinnavarro16@gmail.com>
2014-08-04 15:37:48 +0800

    updated urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,5 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls', namespace='polls'))
+    url(r'^polls/', include('polls.urls', namespace='polls')),
 )
