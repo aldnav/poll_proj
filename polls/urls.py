2ce55497ebcade643ab5883a0d8461199b14e2d1
aldnav <aldrinnavarro16@gmail.com>
2014-08-04 11:48:36 +0800

    Added urls to index, details and results

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from polls import views
+
+urlpatterns = patterns('',
+	#/polls/
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	#/polls/1
+	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+	#/polls/1/results
+	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+	#/polls/1/vote
+	url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+)
