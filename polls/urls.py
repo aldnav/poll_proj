5f12eea138528f43517e0702dc87d025dce578e1
aldnav <aldrinnavarro16@gmail.com>
2014-08-04 15:39:57 +0800

    polls/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
 	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 	#/polls/1/vote
 	url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+	#ajax vote
+	url(r'^(?P<poll_id>\d+)/ajaxvote/$', views.ajax_vote, name='voteajax'),
 )
