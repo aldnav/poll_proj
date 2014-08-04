dea57cd37515aa74c089745b57c0deb52088826b
aldnav <aldrinnavarro16@gmail.com>
2014-08-04 15:41:44 +0800

    added views for ajax_vote

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.views import generic
 import time
+import json
 
 from polls.models import Poll, Choice
 
@@ -25,6 +26,18 @@ class ResultsView(generic.DetailView):
 	model = Poll
 	template_name = 'polls/results.html'
 
+class JSONResponseMixin(object):
+	def render_to_response(self, context):
+		return self.get_json_response(self.convert_context_to_json(context))
+
+	def get_json_response(self, content, **httpresponse_kwargs):
+		return HttpResponse(content,
+							content_type='application/json',
+							**httpresponse_kwargs)
+
+	def convert_context_to_json(self, context):
+		return json.dumps(context)
+
 def vote(request, poll_id):
 	poll = get_object_or_404(Poll, pk=poll_id)
 	try:
@@ -39,3 +52,18 @@ def vote(request, poll_id):
 		selected_choice.save()
 
 		return HttpResponseRedirect(reverse('polls:results', args=(poll.id, )))
+
+def ajax_vote(request, poll_id):
+	poll = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'poll': poll,
+			'error_message': 'Please select a valid choice!'
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+
+		return HttpResponseRedirect(poll.get_absolute_url())
