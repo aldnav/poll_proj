9394c1afc5fdb4fb3a48799d19c731c44191aee9
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-08-05 16:38:30 +0800

    Added config for email

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,3 +82,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Settings for email service
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'aldrinnavarro16@gmail.com'
+EMAIL_HOST_PASSWORD = 'zyxelnavarroclanz16'
