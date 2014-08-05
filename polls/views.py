ef40f5219f651bb46b7f16bc9dda48f0987a7976
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-08-05 13:43:54 +0800

    Added contact functionality

--- polls/views.py
+++ polls/views.py
@@ -7,63 +7,62 @@ import time
 import json
 
 from polls.models import Poll, Choice
+from polls.forms import ContactForm
 
 class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_polls'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_polls'
 
-	def get_queryset(self):
-		return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-	model = Poll
-	template_name = 'polls/detail.html'
+    model = Poll
+    template_name = 'polls/detail.html'
 
-	def get_queryset(self, *args, **kwargs):
-		return Poll.objects.filter(pub_date__lte=timezone.now())
+    def get_queryset(self, *args, **kwargs):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
-	model = Poll
-	template_name = 'polls/results.html'
-
-class JSONResponseMixin(object):
-	def render_to_response(self, context):
-		return self.get_json_response(self.convert_context_to_json(context))
-
-	def get_json_response(self, content, **httpresponse_kwargs):
-		return HttpResponse(content,
-							content_type='application/json',
-							**httpresponse_kwargs)
-
-	def convert_context_to_json(self, context):
-		return json.dumps(context)
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
-	poll = get_object_or_404(Poll, pk=poll_id)
-	try:
-		selected_choice = poll.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', {
-			'poll': poll,
-			'error_message': 'Please select a valid choice!'
-		})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': poll,
+            'error_message': 'Please select a valid choice!'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
 
-		return HttpResponseRedirect(reverse('polls:results', args=(poll.id, )))
+        return HttpResponseRedirect(reverse('polls:results', args=(poll.id, )))
 
-def ajax_vote(request, poll_id):
-	poll = get_object_or_404(Poll, pk=poll_id)
-	try:
-		selected_choice = poll.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', {
-			'poll': poll,
-			'error_message': 'Please select a valid choice!'
-		})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # process data here
+            print form.cleaned_data
+            # subject = form.cleaned_data['subject']
+            # message = form.cleaned_data['message']
+            # sender = form.cleaned_data['sender']
+            # cc_myself = form.cleaned_data['cc_myself']
 
-		return HttpResponseRedirect(poll.get_absolute_url())
+            # recipients = ['info@example.com']
+            # if cc_myself:
+            #     recipients.append(sender)
+
+            # from django.core.mail import send_mail
+            # send_mail(subject, message, sender, recipients)
+            return HttpResponseRedirect('/')
+    else:
+        form = ContactForm()
+
+    return render(request, 'polls/contact.html', {
+        'form':form
+    })
