d8ada26f8f779b7661d11212bda1cfe1a89dd8c5
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-08-05 16:39:29 +0800

    Added contact function

--- polls/views.py
+++ polls/views.py
@@ -48,17 +48,17 @@ def contact(request):
         if form.is_valid():
             # process data here
             print form.cleaned_data
-            # subject = form.cleaned_data['subject']
-            # message = form.cleaned_data['message']
-            # sender = form.cleaned_data['sender']
-            # cc_myself = form.cleaned_data['cc_myself']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
 
-            # recipients = ['info@example.com']
-            # if cc_myself:
-            #     recipients.append(sender)
+            recipients = ['info@example.com']
+            if cc_myself:
+                recipients.append(sender)
 
-            # from django.core.mail import send_mail
-            # send_mail(subject, message, sender, recipients)
+            from django.core.mail import send_mail
+            send_mail(subject, message, sender, recipients)
             return HttpResponseRedirect('/')
     else:
         form = ContactForm()
